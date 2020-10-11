7eb7764752443472e734ae99ff6e541fe00dcd9f
Lucas Testa <lucas.testa2222@gmail.com>
2020-10-11 14:46:35 +0200

    testing row and col

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,175 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+  const initGrid = [
+    [["v"], ["v"], ["v"], ["v"], ["v"], ["v"], ["v"]],
+    [["v"], ["v"], ["v"], ["v"], ["v"], ["v"], ["v"]],
+    [["v"], ["v"], ["v"], ["v"], ["v"], ["v"], ["v"]],
+    [["v"], ["v"], ["v"], ["v"], ["v"], ["v"], ["v"]],
+    [["v"], ["v"], ["v"], ["v"], ["v"], ["v"], ["v"]],
+    [["v"], ["v"], ["v"], ["v"], ["v"], ["v"], ["v"]],
+  ];
+  const [grid, setGrid] = useState(initGrid);
+  const [player, setPlayer] = useState("r");
+  const [winner, setWinner] = useState("v");
+
+  const renderedGrid = (grid) => {
+    const test = grid.map((e) => (
+      <div className="row">
+        {e.map((ee) => {
+          if (ee == "r") {
+            return <div className="cell red"></div>;
+          } else if (ee == "y") {
+            return <div className="cell yellow"></div>;
+          } else {
+            return <div className="cell"></div>;
+          }
+        })}
+      </div>
+    ));
+    return test;
+  };
+
+  const renderedPlayZone = (player) => {
+    let arr = [];
+    for (let i = 0; i < 7; i++) {
+      if (grid[0][i] == "v" && winner == "v") {
+        if (player == "r") {
+          arr.push(
+            <div
+              className="selector red"
+              onClick={() => {
+                setPlayer("y");
+                played(i);
+              }}
+            ></div>
+          );
+        } else {
+          arr.push(
+            <div
+              className="selector yellow"
+              onClick={() => {
+                setPlayer("r");
+                played(i);
+              }}
+            ></div>
+          );
+        }
+      } else {
+        arr.push(<div className="selector"></div>);
+      }
+    }
+    return <div id="playZone">{arr}</div>;
+  };
+
+  const played = (col) => {
+    let modif = grid;
+    for (let j = 5; j >= 0; j--) {
+      if (modif[j][col] == "v") {
+        modif[j][col] = player;
+        setGrid(modif);
+        testVictory(modif);
+        return;
+      }
+    }
+  };
+
+  const testVictory = (gridTest) => {
+    let test = ["v", 0];
+    for (let i = 0; i < 7; i++) {
+      test = ["v", 0];
+      for (let j = 0; j < 6; j++) {
+        if (gridTest[j][i] == test[0] && gridTest[j][i] != "v") {
+          test[1] += 1;
+        } else {
+          test[0] = gridTest[j][i];
+          test[1] = 0;
+        }
+        if (test[1] == 3) {
+          setWinner(test[0]);
+          break;
+        }
+      }
+    }
+    for (let i = 0; i < 6; i++) {
+      test = ["v", 0];
+      for (let j = 0; j < 7; j++) {
+        if (gridTest[i][j] == test[0] && gridTest[i][j] != "v") {
+          test[1] += 1;
+        } else {
+          test[0] = gridTest[i][j];
+          test[1] = 0;
+        }
+        if (test[1] == 3) {
+          setWinner(test[0]);
+          break;
+        }
+      }
+    }
+    for (let i=0 ; i<6 ;i++){
+      test = ["v", 0];
+    
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div id="container">
+      <style jsx global>
+        {`
+          .row {
+            display: flex;
+            justify-content: space-between;
+          }
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          .cell {
+            background-color: white;
+            width: 80px;
+            height: 80px;
+            border: 5px solid blue;
+            border-radius: 50%;
+          }
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          .red {
+            background-color: red;
+          }
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          .yellow {
+            background-color: yellow;
+          }
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          #board {
+            background-color: blue;
+            color: white;
+            width: min-content;
+            padding: 20px;
+          }
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          #playZone {
+            display: flex;
+            margin: 20px;
+          }
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+          .selector {
+            height: 80px;
+            width: 80px;
+            background-color: white;
+            border: 5px solid black;
+            border-radius: 50%;
+          }
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          .red:hover {
+            background-color: red;
+          }
+
+          .yellow:hover {
+            background-color: yellow;
+          }
+        `}
+      </style>
+      {winner}
+      {renderedPlayZone(player)}
+      <div id="board">{renderedGrid(grid)}</div>
     </div>
-  )
+  );
 }
